9bc0737d436955b7aba39d3f05f23237222f3dab
TheEnderCore <theendercore@gmail.com>
2022-04-27 19:33:24 +0300

    did things

--- index.ts
+++ index.ts
@@ -1,25 +1,82 @@
-import DiscordJS, { Intents, Message } from 'discord.js'
+import DiscordJS, { Intents, Message, Options } from 'discord.js'
 import dotenv from "dotenv"
 dotenv.config()
 
 const client = new DiscordJS.Client({
-    intents:[
+    intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES
     ]
 })
 
 client.on('ready', () => {
-    console.log("WaffBot up")
+    console.log(`Logged in as `+client.user?.tag)
+
+    const guildID = '968877307638997032'
+    const guild = client.guilds.cache.get(guildID)
+    let commands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'replies with pong'
+    })
+
+
+    commands?.create({
+        name: 'add',
+        description: 'adds things :)',
+        options: [
+            {
+            name: '1th',
+            description: 'the first',
+            required: true,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.STRING
+           },
+           {
+           name: '2th',
+           description: 'the seconth',
+           required: true,
+           type: DiscordJS.Constants.ApplicationCommandOptionTypes.INTEGER
+          }
+        ]
+    })
 })
 
-client.on('messageCreate', (message) => {
-    if(message.content === 'ping') {
-        message.reply({
-            content:'pong',
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()){
+        return
+    }
+
+    const {commandName, options} = interaction
+
+    if (commandName === 'ping'){
+        interaction.reply({
+            content: 'pong',
+        })
+    } else if (commandName === 'add'){
+        const text = options.getString('1th')!
+        const numb = options.getInteger('2th')!
+
+        interaction.reply({
+            content: `Lmao no this is multipay : ${text + numb}`
         })
     }
 })
+
+
+// client.on('messageCreate', (message) => {
+//     if(message.content === 'ping') {
+//         message.reply({
+//             content:'pong',
+//         })
+//     }
+// })
 
 
 client.login(process.env.TOKEN)
