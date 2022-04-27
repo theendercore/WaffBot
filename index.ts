04e3d98d763fe27f2d93f4868a1287ea684b78d4
TheEnderCore <theendercore@gmail.com>
2022-04-27 17:51:30 +0300

    test things

---
+++ index.ts
@@ -0,0 +1,25 @@
+import DiscordJS, { Intents, Message } from 'discord.js'
+import dotenv from "dotenv"
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents:[
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log("WaffBot up")
+})
+
+client.on('messageCreate', (message) => {
+    if(message.content === 'ping') {
+        message.reply({
+            content:'pong',
+        })
+    }
+})
+
+
+client.login(process.env.TOKEN)
