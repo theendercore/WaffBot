a96dc4cbc6b0ca7e3242c8a8f1c253abe052a525
TheEnderCore <theendercore@gmail.com>
2022-04-28 19:27:24 +0300

    yes

--- commands/ping.ts
+++ commands/ping.ts
@@ -2,7 +2,7 @@ import { ICommand } from 'wokcommands'
 
 export default {
   category: 'Testing',
-  description: 'Replies with pong', // Required for slash commands
+  description: 'Replies with cock', // Required for slash commands
   
   slash: true,
   testOnly: true,
