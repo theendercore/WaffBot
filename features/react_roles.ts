0dfde26b3aa14a08e432d6d78c2f816dfce00248
TheEnderCore <theendercore@gmail.com>
2022-05-09 17:16:53 +0300

    .

--- features/react_roles.ts
+++ features/react_roles.ts
@@ -3,16 +3,18 @@ import WOKCommands from 'wokcommands'
 import log from '../common/log'
 
 export default (client: Client, instance: WOKCommands) => {
-  client.on('messageReactionAdd', (mesage) =>{
-    // log(mesage.emoji+' ')
-      // console.log('[WafBot] '+ member.nickname)
+  client.on('messageReactionAdd', (reaction, user) => {
+    log(reaction.emoji.name + ' ' + reaction.emoji.identifier+ ' '+ reaction.me)
+    
+    reaction.remove()
+    
+
   })
 }
 
 const config = {
-    displayName: 'test',
-    dbName: 'TEST'
-  }
-  
-  export { config }
-  
+  displayName: 'test',
+  dbName: 'TEST'
+}
+
+export { config }
