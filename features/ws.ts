a75133ea6a5b3aa97ef23a0fd161efb1926bca51
theendercore <theendercore@gmail.com>
2022-06-02 17:50:08 +0300

    cleaned up the ws and made them 90% done

--- features/ws.ts
+++ features/ws.ts
@@ -1,15 +1,22 @@
 import { Client } from "discord.js";
 import log from "../common/log";
 import WebSocket from "ws";
+import { varifyRole, waitingVarifyRole } from "../common/vars";
 
 export default async (client: Client) => {
   const wss = new WebSocket.Server({ port: 8080 });
 
   wss.on("connection", (ws) => {
     log("on");
-    ws.on("message", (d) => {
+    ws.on("message", async (d) => {
       let data = JSON.parse(d.toString());
-      console.log(`Client data : ${data.PP} | ${d.toString()}`);
+      console.log(`Client data : ${data.server} | ${d.toString()}`);
+
+      // var guild = client.guilds.cache.get(data.server);
+      // const member = await guild?.members.fetch(data.user);
+      // log("A member has verified id: " + member?.user.tag);
+      // member?.roles.remove(waitingVarifyRole);
+      // member?.roles.add(varifyRole);
     });
   });
 };
