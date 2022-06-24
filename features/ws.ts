a0bf9bcb8b0e8f8da78abe8d33dc187eea63530c
theendercore <theendercore@gmail.com>
2022-06-24 16:12:20 +0300

    .

--- features/ws.ts
+++ features/ws.ts
@@ -10,7 +10,7 @@ export default async (client: Client) => {
   // @ts-ignore
   const wss = new WebSocket.Server({ port: 8080 });
 
-  log(wss)
+  log(toString(wss))
 
   wss.on("connection", (ws) => {
     log("Verify Server Connected");
